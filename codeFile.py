c84adabb814db18074fa8ee006c7fd705aa9429b
siddhartha-12 <siddhartharaju12@gmail.com>
2021-06-22 02:31:10 -0400

    Adding code to display day, data and time

--- codeFile.py
+++ codeFile.py
@@ -2,7 +2,6 @@ import datetime
 
 if __name__ == "__main__":
     now = datetime.datetime.now()
-    print ("Current time : ")
-    print (now.strftime("%Y-%m-%d %H:%M:%S"))
-
+    print ("Current Dat, Date and time : ")
+    print (now.strftime("%A %Y-%m-%d %H:%M:%S"))
 
