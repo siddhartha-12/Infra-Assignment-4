d1e86f44fddb976dce4f5c6425a3e39fd3242ccb
siddhartha-12 <siddhartharaju12@gmail.com>
2021-06-22 02:23:17 -0400

    Adding code to display data and time

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Infra-Assignment-4
+
+### Name: Siddhartha Raju
+### NUID: 001084614
+### Email: raju.s@northeastern.edu
+### Department: MGEN- Cyber-Physical Systems
+### University: Northeastern University
+
--- codeFile.py
+++ codeFile.py
@@ -3,6 +3,6 @@ import datetime
 if __name__ == "__main__":
     now = datetime.datetime.now()
     print ("Current time : ")
-    print (now.strftime("%H:%M:%S"))
+    print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
 
